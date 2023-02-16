d887ee7a1866c4564f18b1fbb093f028bdeb8b49
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2023-02-16 22:51:57 +0100

    Setup the basic feature for search

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,6 +1,7 @@
+var start = 0;
+var limit = 15;
 
-$("#search_form").submit(function (e) {
-	e.preventDefault();
+async function get_search(start=0) {
 	var form_data = $('#search_form').serializeArray();
 	var values = {};
 	$.each(form_data, function (i, field) {
@@ -9,39 +10,73 @@ $("#search_form").submit(function (e) {
 
 	//Value Retrieval Function
 	var getValue = function (valueName) {
-		return values[valueName];
+		return values[valueName] || false;
 	};
 
-	//Retrieve the Values
-	var url = 'https://wattpad-extension.vercel.app/search/$search_word'.replace("$search_word", getValue('search'));
-	$.ajax({
-		type: 'POST',
-		url: url,
-		data: values,
-		success: function(data) {
-			console.log(data);
-			$("#search_form").after("<div id='search_results'></div>");
-			$(data).each(function(i,val){
-				$("#search_results").append(
-					`<div class="a-box py-3">
-					<div class="img-container">
-					  <div class="img-inner">
-						<div class="inner-skew">
-						  <img src="${val.img_url}}">
-						</div>
-					  </div>
-					</div>
-					<div class="text-container">
-					  <h3>${val.title}</h3>
-					  <div>
-							${val.description}
-					  </div>
-				  </div>`
-				)
-				console.log(i,val)
-			  });
+	let query = getValue('search');
+	
+	// Filters
+	const filter = {
+		"complete" : getValue('completed'),
+		"mature" : getValue('mature'),
+		"free" : getValue('free'),
+		"paid" : getValue('paid'),
+		"start" : start,
+		"limit" : 15,
+	}
+	const response = await fetch(`https://wattpad-extension.vercel.app/search/${query}`, {
+		method: 'POST',
+		headers: {
+			'accept': 'application/json',
+			'Content-Type': 'application/json'
 		},
-		fail: function(data) {
-			console.log("No Connection");
-		}
-})});
+		body: JSON.stringify(filter)
+	});
+	const response_data = await response.json();
+	if (response.status === 200) {
+		$(response_data).each(function(i,val){
+			$("#search_results").append(
+				`<div class="a-box py-3">
+				<div class="img-container">
+					<div class="img-inner">
+					<div class="inner-skew">
+						<img src="${val.img_url}}">
+					</div>
+					</div>
+				</div>
+				<div class="text-container">
+					<h3>${val.title}</h3>
+					<p class="text-break">
+						${val.description}
+					</p>
+				</div>`
+			)
+		});
+		$("#search_results").after(`
+		<div class="text-center">
+			<button id="more_result" type="button" class="btn bg-custom btn-sm">See more results</button>
+		</div>
+		`);
+	} else {
+		console.log("No Connection");
+	}
+
+
+};
+
+
+$("#search_form").submit(function (e) {
+	e.preventDefault();
+	get_search();
+});
+
+
+$(document).on('click', '#more_result', function(e) {
+	e.preventDefault();
+	start = limit + 1;
+	get_search(result_start=start);
+	console.log(start);
+	$("#more_result").remove();
+});
+document.body.style.width = '350px';
+// document.documentElement.style.height = "10rem";
